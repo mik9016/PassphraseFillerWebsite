0e59aa3909f1b8e8fb2ba236bab2dd73974076bf
Mikolaj Gruszecki <mikolaj.gruszecki@sdui.de>
2024-06-16 00:28:13 +0200

    remove logs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const githubUrl = 'https://api.github.com/repos/mik9016/PassphraseFillerChrome/releases/latest'
 fetch(githubUrl).then(res => res.json()).then(
     data => {
-        console.log(data)
         const latestVersionTag = document.getElementById('latestVersion')
         const chromiumDownloadLink = document.getElementById('chromiumDownloadLink')
         latestVersionTag.innerText = `Latest version: ${data?.tag_name}`
